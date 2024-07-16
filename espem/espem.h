55d2de7a7875f161b7b61cc4624ab47c8b80ec62
hosang lee <2540@paran.com>
2024-07-16 09:09:49 +0900

    /////

--- espem/espem.h
+++ espem/espem.h
@@ -45,21 +45,22 @@ extern Scheduler ts;
 // #include "espem.h"
 #include "EmbUI.h"	// EmbUI framework
 
-#define MAX_FREE_MEM_BLK ESP.getMaxAllocHeap()
-#define PUB_JSSIZE		 1024
+#define 			MAX_FREE_MEM_BLK			ESP.getMaxAllocHeap()
+#define 			PUB_JSSIZE					1024
 // sprintf template for json sampling data
-#define JSON_SMPL_LEN	 85	 // {"t":1615496537000,"U":229.50,"I":1.47,"P":1216,"W":5811338,"hz":50.0,"pF":0.64},
-static const char  PGsmpljsontpl[] PROGMEM = "{\"t\":%u000,\"U\":%.2f,\"I\":%.2f,\"P\":%.0f,\"W\":%.0f,\"hz\":%.1f,\"pF\":%.2f},";
-static const char  PGdatajsontpl[] PROGMEM = "{\"age\":%llu,\"U\":%.1f,\"I\":%.2f,\"P\":%.0f,\"W\":%.0f,\"hz\":%.1f,\"pF\":%.2f}";
+#define 			JSON_SMPL_LEN				85	 	// {"t":1615496537000,"U":229.50,"I":1.47,"P":1216,"W":5811338,"hz":50.0,"pF":0.64},
+static const char	PGsmpljsontpl[] PROGMEM 	= "{\"t\":%u000,\"U\":%.2f,\"I\":%.2f,\"P\":%.0f,\"W\":%.0f,\"hz\":%.1f,\"pF\":%.2f},";
+static const char	PGdatajsontpl[] PROGMEM 	= "{\"age\":%llu,\"U\":%.1f,\"I\":%.2f,\"P\":%.0f,\"W\":%.0f,\"hz\":%.1f,\"pF\":%.2f}";
 
 // HTTP responce messages
-static const char  PGsmpld[]			= "Metrics collector disabled";
-static const char  PGdre[]			= "Data read error";
-static const char  PGacao[]		        = "Access-Control-Allow-Origin";
-static const char *PGmimetxt			= "text/plain";
+static const char  PGsmpld[]					= "Metrics collector disabled";
+static const char  PGdre[]						= "Data read error";
+static const char  PGacao[]		        		= "Access-Control-Allow-Origin";
+static const char *PGmimetxt					= "text/plain";
 // static const char* PGmimehtml = "text/html; charset=utf-8";
+
 /////////////////
-void			 block_menu(Interface *interf);
+void block_menu(Interface *interf);
 /**
  * @brief callback method to print debug data on PZEM RX
  *
